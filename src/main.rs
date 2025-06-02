c1795e90363df89802b4935c5a2237fc8a245d2d
Bernd Krietenstein <bernd.krietenstein@gmx.de>
2025-06-02 13:07:33 +0200

    Removed profiling code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use pprof::ProfilerGuard;
-
 use crate::app::App;
 
 pub mod app;
@@ -8,26 +6,10 @@ pub mod ui;
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
-    let guard = if std::env::var("NETVIEW_PROFILE").is_ok() {
-        Some(ProfilerGuard::new(100).unwrap()) // 100 Hz sampling
-    } else {
-        None
-    };
-
     color_eyre::install()?;
     crossterm::terminal::enable_raw_mode()?;
     let terminal = ratatui::init();
     let result = App::new().run(terminal).await;
     ratatui::restore();
-
-    if let Some(guard) = guard {
-        if let Ok(report) = guard.report().build() {
-            let file = std::fs::File::create("flamegraph.svg").unwrap();
-            report.flamegraph(file).unwrap();
-            println!("Flamegraph written to flamegraph.svg");
-        } else {
-            println!("Failed to build pprof report");
-        }
-    }
     result
 }
